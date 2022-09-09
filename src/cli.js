50f5b68ab9f0b6445f5f798765581c5f430c2573
Denis Tankov <denis_tankoff@mail.ru>
2022-09-09 08:12:02 +0700

    fixed errors

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const greetings = () => {
-  
+const greetings = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log('Hello, ' + userName + '!');
-}
+  console.log(`${'Hello'}, ${userName}!`);
+};
+
+export default greetings;
